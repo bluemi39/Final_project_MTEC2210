0b2fde6897be6ffef506238a2575495229e3a67e
bluemi39 <47123010+bluemi39@users.noreply.github.com>
2020-12-03 19:42:18 -0500

    Unity files

---
+++ MTEC2210_FP/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public float ForwardForce = 10f;
+
+    public Transform rb;
+
+    private AudioSource audioSource;
+
+    public AudioClip [] soundClips;
+    // 0 = Shooting
+    // 1 = Explosion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        audioSource = GetComponent<AudioSource>();
+
+    }
+
+    void FixedUpdate ()
+    {
+        
+
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(ForwardForce * Time.deltaTime, 0, 0);
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+            FireBullet();
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+
+            TriggerExplosion();
+
+        }
+
+
+    }
+
+    void TriggerExplosion()
+    {
+        audioSource.PlayOneShot(soundClips[1]);
+
+        audioSource.panStereo = 0;
+
+    }
+
+    void FireBullet()
+    {
+        //Code that fires projectile bullet
+
+        //Play sound
+        audioSource.PlayOneShot(soundClips[0]);
+    }
+}
