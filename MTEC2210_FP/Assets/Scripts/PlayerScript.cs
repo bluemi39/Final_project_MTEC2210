7d28a175339685816f1e2376d022538dc64e8e52
bluemi39 <47123010+bluemi39@users.noreply.github.com>
2020-12-20 21:58:07 -0500

    Change to the project
    Added boundaries to prevent player from getting out of camera view.

--- MTEC2210_FP/Assets/Scripts/PlayerScript.cs
+++ MTEC2210_FP/Assets/Scripts/PlayerScript.cs
@@ -8,6 +8,12 @@ public class PlayerScript : MonoBehaviour
     private AudioSource audioSource;
 
     public float range = 100f;
+    
+    private Transform player;
+
+    public float speed;
+    
+    public float maxBound, minBound;
 
     // public GameObject PlayerShooter;
 
@@ -18,29 +24,35 @@ public class PlayerScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        player = GetComponent<Transform> ();
 
         audioSource = GetComponent<AudioSource>();
 
     }
 
-    void FixedUpdate ()
-    { 
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.Translate(10f * Time.deltaTime, 0f, 0f);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Translate(-10f * Time.deltaTime, 0f, 0f);
-        }
+   // void FixedUpdate ()
+   // { 
+     //   if (Input.GetKey(KeyCode.D))
+       // {
+        //    transform.Translate(10f * Time.deltaTime, 0f, 0f);
+        //}
+        //if (Input.GetKey(KeyCode.A))
+        //{
+        //    transform.Translate(-10f * Time.deltaTime, 0f, 0f);
+        //}
 
-    }
+    // }
 
     // Update is called once per frame
     void Update()
     {
+        float h = Input.GetAxis("Horizontal");
 
-
+        if (player.position.x < minBound && h < 0)
+            h = 0;
+        else if (player.position.x > maxBound && h > 0)
+            h = 0;
+        player.position += Vector3.right * h * speed;
         if (Input.GetKeyDown(KeyCode.Space))
         {
 
